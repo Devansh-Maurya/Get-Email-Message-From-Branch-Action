09a129684a7e6f94f2986c305957df3df121433e
Devansh Maurya <Devansh233@gmail.com>
2020-09-15 08:37:48 +0530

    Send email directly

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const core = require('@actions/core');
 const github = require('@actions/github');
-const sgMail = require('@sendgrid/mail');
 
 try {
     const branch = core.getInput('branch-name');
@@ -29,25 +28,6 @@ try {
 
     core.setOutput('subject', subject);
     core.setOutput('body', body);
-
-    var apiKey = core.getInput('sendgrid-api-key');
-    var senderEmail = core.getInput('sender-email');
-
-    console.log(senderEmail)
-
-    sgMail.setApiKey(apiKey);
-
-    const msg = {
-        to: 'devansh233@gmail.com',
-        from: senderEmail,
-        subject: subject,
-        text: body
-    };
-    
-    sgMail
-        .send(msg)
-        .then(() => console.log('Mail sent successfully'))
-        .catch(error => console.error(error.toString()));
 
 } catch (error) {
     console.error(error.message);
